0fe7ecb551dd53725a4e64f9863837cfbb818bcb
Pablito <pablitothechicken@outlook.com>
2025-08-07 14:00:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const crypto   = require('crypto');
 
 const CLIENT_ID     = process.env.ROBLOX_CLIENT_ID     || '2502928924991748390';
 const CLIENT_SECRET = process.env.ROBLOX_CLIENT_SECRET || 'RBX-bf1Hc1QNDkWZfpWCeSRUvgwFhsaJv0Mej-QfVuFboHcUILqdCkvUmvWoOUs89G3I';
-const REDIRECT_URI  = process.env.ROBLOX_REDIRECT_URI  || 'http://localhost:3000/auth/roblox/callback';
-const PORT          = process.env.PORT || 3000;
+const REDIRECT_URI  = process.env.ROBLOX_REDIRECT_URI  || 'http://localhost:80/auth/roblox/callback';
+const PORT          = process.env.PORT || 80;
 
 const app = express();
 app.use(session({
@@ -127,6 +127,6 @@ app.get('/logout', (req, res) => {
   });
 });
 
-app.listen(port, '45.143.196.245', () => {
+app.listen(PORT, '45.143.196.245', () => {
     console.log(`Server is running on Cahoots.gg`);
 });
