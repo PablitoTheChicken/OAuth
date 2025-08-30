1a139760e0d15d5ebad6d5e9304de9131198d857
Pablito <pablitothechicken@outlook.com>
2025-08-30 17:11:53 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ router.use('/auth', require('./auth'));
 router.use('/game', require('./game'));
 router.use('/user', require('./user'));
 router.use('/editor', require('./editor'));
+router.use('/mail', require('./mail'));
 
 router.get('/', (req, res) => {
   res.json({ message: 'Ok' });
